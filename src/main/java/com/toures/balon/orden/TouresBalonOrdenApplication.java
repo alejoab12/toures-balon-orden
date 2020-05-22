5a44d4958991b5f189fd10557e31f545e8c0d4e1
Manuel alejandro alcala <alejoab12@hotmail.com>
2020-05-22 14:11:52 -0500

    Se agregan fuentes

--- src/main/java/com/toures/balon/orden/TouresBalonOrdenApplication.java
+++ src/main/java/com/toures/balon/orden/TouresBalonOrdenApplication.java
@@ -2,10 +2,12 @@ package com.toures.balon.orden;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableDiscoveryClient
 public class TouresBalonOrdenApplication {
 
     public static void main(String[] args) {
